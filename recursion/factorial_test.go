cc59ed49bb5ba04b314481ac5f8c9c3f5befec25
clinton <clintonwanason1@gmail.com>
2018-08-29 22:19:28 +0800

    added test for factorial recursion

--- recursion/factorial_test.go
+++ recursion/factorial_test.go
@@ -1,1 +1,26 @@
 package recursion
+
+import (
+	"testing"
+
+	"github.com/clintonpuge/go-ds/helpers"
+)
+
+// TestFactorial factorial recursion
+func TestFactorial(t *testing.T) {
+	tests := []struct {
+		num    int
+		result int
+	}{
+		{num: 5, result: 120},
+		{num: 6, result: 720},
+	}
+	for _, test := range tests {
+		actual := Factorial(test.num)
+		if actual != test.result {
+			t.Errorf("%v Should return %d instead got %d", helpers.Xmark, test.result, actual)
+		} else {
+			t.Logf("Should receive %d %v", test.result, helpers.CheckMark)
+		}
+	}
+}
