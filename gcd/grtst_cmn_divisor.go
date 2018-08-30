3350a12074f79fc359a247353b2b3a39492e7aca
clinton <clintonwanason1@gmail.com>
2018-08-30 23:08:47 +0800

    added gcd functionality

--- gcd/grtst_cmn_divisor.go
+++ gcd/grtst_cmn_divisor.go
@@ -1,6 +1,12 @@
 package gcd
 
 // GreatestCommonDivisor func
-func GreatestCommonDivisor(n int, m int) int {
-	return 1
+func GreatestCommonDivisor(m int, n int) int {
+	if m < n {
+		return GreatestCommonDivisor(n, m)
+	}
+	if m%n == 0 {
+		return n
+	}
+	return GreatestCommonDivisor(n, m%n)
 }
