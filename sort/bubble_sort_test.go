e7d6ec6646cac14e7de1d0e3161398bd5d5e51f1
clinton <clintonwanason1@gmail.com>
2018-09-08 17:00:38 +0800

    added bubble sort test

---
+++ sort/bubble_sort_test.go
@@ -0,0 +1,23 @@
+package sort
+
+import (
+	"testing"
+)
+
+// TestBinarySearch test
+func TestBubbleSort(t *testing.T) {
+	tests := []struct {
+		data   []int
+		result []int
+	}{
+		{data: []int{8, 3, 5, 4, 7, 9}, result: []int{3, 4, 5, 7, 8, 9}},
+		{data: []int{8, 2, 5, 4, 7, 9, 1, 3}, result: []int{1, 2, 3, 4, 5, 7, 8, 9}},
+	}
+
+	for _, test := range tests {
+		actual := BubbleSort(test.data, test.value)
+		if actual != test.result {
+			t.Errorf("It should return %v instead got %v", test.result, actual)
+		}
+	}
+}
