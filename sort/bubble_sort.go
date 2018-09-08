a93364e31c3f186d856f80f285128838302a92e8
clinton <clintonwanason1@gmail.com>
2018-09-08 20:48:28 +0800

    bubble sort removed temp

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -5,9 +5,7 @@ func BubbleSort(numbers []int) {
 	for i := 0; i < len(numbers)-1; i++ {
 		for j := 0; j < len(numbers)-i-1; j++ {
 			if numbers[j] > numbers[j+1] {
-				temp := numbers[j]
-				numbers[j] = numbers[j+1]
-				numbers[j+1] = temp
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 			}
 		}
 	}
