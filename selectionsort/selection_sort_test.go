bc6570caefb9a4d90e91e41d92b1648feebf7362
clinton <clintonwanason1@gmail.com>
2018-09-09 11:42:00 +0800

    added selection sort test

---
+++ selectionsort/selection_sort_test.go
@@ -0,0 +1,24 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+// TestSelectionSort test
+func TestSelectionSort(t *testing.T) {
+	tests := []struct {
+		data   []int
+		result []int
+	}{
+		{data: []int{8, 3, 5, 4, 7, 9}, result: []int{3, 4, 5, 7, 8, 9}},
+		{data: []int{8, 2, 5, 4, 7, 9, 1, 3}, result: []int{1, 2, 3, 4, 5, 7, 8, 9}},
+	}
+
+	for _, test := range tests {
+		SelectionSort(test.data)
+		if !reflect.DeepEqual(test.data, test.result) {
+			t.Errorf("It should return %v instead got %v", test.result, test.data)
+		}
+	}
+}
